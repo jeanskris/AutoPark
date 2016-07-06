9226e779ee2d067aece35a5a8d5ec2b7671312ca
jeanskris <823509241@qq.com>
2016-07-06 11:10:38 +0800

    coordinate data type: int  to  double

--- park-model/src/main/java/com/SCC/park/model/Coordinate.java
+++ park-model/src/main/java/com/SCC/park/model/Coordinate.java
@@ -6,6 +6,9 @@ package com.SCC.park.model;
 public class Coordinate {
     private double x;
     private double y;
+    public Coordinate(){
+
+    }
     public Coordinate(double x,double y){
         this.x=x;
         this.y=y;
