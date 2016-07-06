4a42d36d50e609349833f1e29f81ea6c4f7ed6ce
jeanskris <823509241@qq.com>
2016-07-06 11:02:41 +0800

    read map, get useful points by contours, filter blank area, show map on web page, show a new point on where i clicked, send the point i clicked to server.

---
+++ park-sensor/src/test/java/TestReadMap.java
@@ -0,0 +1,13 @@
+import com.SCC.park.sensor.PerceptService;
+
+/**
+ * Created by ZJDX on 2016/7/4.
+ */
+
+public class TestReadMap {
+
+    public static void main(String[] args){
+        PerceptService perceptService=new PerceptService();
+        perceptService.getMapByOpencv();
+    }
+}
